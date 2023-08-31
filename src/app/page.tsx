cfae2d382a015cf1e033ca5a529e31e6ad6c63b6
BernardCooley <bernardcooley@gmail.com>
2023-08-31 11:38:09 +0100

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import { Center, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("/review");
+    }, []);
+
     return (
         <main></main>
     );
