0395134dac95fe4a75bdd57e7ce5266879e1c5eb
BernardCooley <bernardcooley@gmail.com>
2023-08-31 10:29:30 +0100

    removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { Stack } from "@chakra-ui/react";
-import styles from "./page.module.css";
-import Track from "@/components/TrackReviewCard";
 
 export default function Home() {
     return (
-        <main className={styles.main}></main>
+        <main></main>
     );
 }
