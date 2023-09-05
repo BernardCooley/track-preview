84a2936d01642046e1b24df4da4b1d4a5df337a0
BernardCooley <bernardcooley@gmail.com>
2023-09-05 21:35:18 +0100

    bff and get release

---
+++ src/bff/bff.ts
@@ -0,0 +1,79 @@
+import { IRelease } from "../../types";
+
+export class GoneError extends Error {
+    statusCode = 410;
+}
+export class NotFoundError extends Error {
+    statusCode = 404;
+}
+export class BadRequestError extends Error {
+    statusCode = 400;
+}
+export class UnauthorisedError extends Error {
+    statusCode = 401;
+}
+export class InternalError extends Error {
+    statusCode = 500;
+}
+
+export const handleFetchErrors = (response: Response) => {
+    switch (response.status) {
+        case 401:
+            throw new UnauthorisedError(response.statusText);
+        case 400:
+            throw new BadRequestError(response.statusText);
+        case 404:
+            throw new NotFoundError(response.statusText);
+        case 410:
+            throw new GoneError(response.statusText);
+        default:
+            null;
+    }
+};
+
+const getRequestInitWithAuth = (
+    method: "GET" | "POST",
+    authorization: string
+): RequestInit => {
+    const requestInit: RequestInit = {
+        method: method,
+        headers: {
+            Accept: "application/json",
+            Authorization: authorization,
+            "Content-Type": "application/json",
+        },
+    };
+    return requestInit;
+};
+
+export const fetchWithErrorHandling = async <T>(
+    endpoint: RequestInfo,
+    method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH",
+    body?: any
+) => {
+    try {
+        const res = await fetch(endpoint, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+        if (res.ok) {
+            return (await res.json()) as T;
+        }
+        handleFetchErrors(res);
+    } catch (e) {
+        throw e;
+    }
+    return null;
+};
+
+export const fetchRelease = async (
+    releaseId: string
+): Promise<IRelease | null> => {
+    return fetchWithErrorHandling<IRelease>(
+        `https://api.discogs.com/releases/${releaseId}`,
+        "GET"
+    );
+};
--- types.ts
+++ types.ts
@@ -12,3 +12,126 @@ export interface ITrack {
     liked: boolean;
     id: string;
 }
+
+export interface IArtist {
+    anv: string;
+    id: number;
+    join: string;
+    name: string;
+    resource_url: string;
+    role: string;
+    tracks: string;
+}
+
+export interface IContributer {
+    resource_url: string;
+    username: string;
+}
+
+export interface IRating {
+    average: number;
+    count: number;
+}
+
+export interface ISubmitter {
+    resource_url: string;
+    username: string;
+}
+
+export interface ICommunity {
+    contributors: IContributer[];
+    data_quality: string;
+    have: number;
+    rating: IRating;
+    status: string;
+    submitter: ISubmitter;
+    want: number;
+}
+
+export interface ICompany {
+    catno: string;
+    entity_type: number;
+    entity_type_name: string;
+    id: number;
+    name: string;
+    resource_url: string;
+}
+
+export interface IFormat {
+    descriptions: string[];
+    name: string;
+    qty: string;
+}
+
+export interface IIdentifier {
+    type: string;
+    value: string;
+}
+
+export interface IImage {
+    height: number;
+    resource_url: string;
+    type: string;
+    uri: string;
+    uri150: string;
+    width: number;
+}
+
+export interface ILabel {
+    catno: string;
+    entity_type: string;
+    id: number;
+    name: string;
+    resource_url: string;
+}
+
+export interface ITracklistTrack {
+    duration: string;
+    position: string;
+    title: string;
+    type_: string;
+}
+
+export interface IVideo {
+    description: string;
+    duration: number;
+    embed: true;
+    title: string;
+    uri: string;
+}
+
+export interface IRelease {
+    title: string;
+    id: number;
+    artists: IArtist[];
+    data_quality: string;
+    thumb: string;
+    community: ICommunity;
+    companies: ICompany[];
+    country: string;
+    date_added: string;
+    date_changed: string;
+    estimated_weight: number;
+    extraartists: IArtist[];
+    format_quantity: number;
+    formats: IFormat[];
+    genres: string[];
+    identifiers: IIdentifier[];
+    images: IImage[];
+    labels: ILabel[];
+    lowest_price: number;
+    master_id: number;
+    master_url: string;
+    notes: string;
+    num_for_sale: number;
+    released: string;
+    released_formatted: string;
+    resource_url: string;
+    series: [];
+    status: string;
+    styles: string[];
+    tracklist: ITracklistTrack[];
+    uri: string;
+    videos: IVideo[];
+    year: number;
+}
