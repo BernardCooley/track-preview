5ef22e6c2c34e4b3e144ccf8d83c520eeb271306
bernard.cooley <bernard.cooley@global.com>
2023-11-29 20:46:07 +0000

    restore db route updated

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,11 +1,18 @@
 "use client";
+import { restoreDatabase } from "@/bff/bff";
 import Header from "@/components/Header";
 import { Box } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 
 interface Props {}
 
 const Settings = ({}: Props) => {
+    useEffect(() => {
+        // (async () => {
+        //     await restoreDatabase();
+        // })();
+    }, []);
+
     return (
         <Box h="full" m={0} px={[4, 8]}>
             <Header />
