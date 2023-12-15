629129e794a80701ba2918eb1f830ad7bdeb091f
bernard.cooley <bernard.cooley@global.com>
2023-12-15 01:12:45 +0000

    foixed typescript ewrror

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -38,7 +38,7 @@ const Loading = ({
                 </Text>
                 <BouncingDotsLoader />
             </Flex>
-            {showLoadingBar && (
+            {progress && showLoadingBar && (
                 <Flex
                     direction="column"
                     alignItems="center"
