49e1a4a0f58d05c4e7163159dfacc7852b15af62
bernardcooley <bernardcooley@gmail.com>
2024-03-23 01:48:38 +0000

    soundcloud link removed

--- src/components/FullTrackLinks.tsx
+++ src/components/FullTrackLinks.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Flex, Icon, Link, Text } from "@chakra-ui/react";
 import { Track } from "../../types";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import { FaSpotify, FaSoundcloud } from "react-icons/fa";
+import { FaSpotify } from "react-icons/fa";
 
 interface Props {
     currentTrack: Track;
@@ -23,18 +23,12 @@ const FullTrackLinks = ({ currentTrack, onLinkClicked }: Props) => {
             title: "Spotify",
             color: "#1DB954",
         },
-        {
-            url: `https://soundcloud.com/search?q=${currentTrack.artist}%20${currentTrack.title}%20${currentTrack.releaseTitle}`,
-            icon: FaSoundcloud,
-            title: "Soundcloud",
-            color: "#ff5500",
-        },
     ];
 
     return (
         <Flex gap={2} w="full" py={2} direction="column" alignItems="center">
             <Text fontSize={[16, 20]}>Listen to the full track</Text>
-            <Flex w="full" justifyContent="space-around" flexWrap="wrap">
+            <Flex w="70%" justifyContent="space-around" flexWrap="wrap">
                 {links.map((link) => (
                     <Link
                         onClick={onLinkClicked}
