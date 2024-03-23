04fb7be4a247e755b8df407d563d33de66551f0f
bernardcooley <bernardcooley@gmail.com>
2024-03-23 01:41:58 +0000

    change landing page loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 
-import { Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Center,
+    ScaleFade,
+    Flex,
+    Text,
+    VStack,
+} from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { useAuthContext } from "../../Contexts/AuthContext";
+import { useCallback, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebase/firebaseInit";
+import BouncingDotsLoader from "@/components/BouncingLoaderDots";
 
 export default function Home() {
+    const [loading, setLoading] = useState(true);
     const { user } = useAuthContext();
     const router = useRouter();
     const listItems = [
@@ -15,74 +28,132 @@ export default function Home() {
         "Manage or buy your liked tracks from the library",
     ];
 
-    return (
-        <Flex h="full" m={0} px={[4, 8]} direction="column" alignItems="center">
-            <Flex
-                mt={8}
-                mb={4}
-                w={["250px", "350px", "550px"]}
-                alignItems="center"
-                height={["80px", "110px", "140px"]}
-                fontSize={["3xl", "4xl", "5xl"]}
-                fontFamily="brand"
-                backgroundImage={"url(/logo_1x.png)"}
-                backgroundSize={["100% 100%"]}
-                backgroundRepeat="no-repeat"
-            >
-                <Text w="full" textAlign="center">
-                    PHONIQUEST
-                </Text>
-            </Flex>
-            <Flex
-                direction="column"
-                alignItems="center"
-                gap={6}
-                p={6}
-                h="full"
-                w="full"
-            >
-                <Text fontSize={["2xl", "3xl"]}>Welcome to Phoniquest</Text>
-                <Flex w="full" maxW="500px" pb={10}>
-                    <VStack gap={6}>
-                        {listItems.map((item) => (
-                            <Flex
-                                alignItems="center"
-                                key={item}
-                                gap={2}
-                                justifyContent="flex-start"
-                                w="full"
-                            >
-                                <Box
-                                    w="30px"
-                                    h="15px"
-                                    minW="30px"
-                                    backgroundImage={"url(/logo_1x.png)"}
-                                    backgroundSize="100% 100%"
-                                    backgroundRepeat="no-repeat"
-                                ></Box>
+    const isUserLoggedIn = useCallback(async () => {
+        onAuthStateChanged(auth, (user) => {
+            if (user && user.email && user.uid) {
+                router.push("/explore");
+            } else {
+                setLoading(false);
+            }
+        });
+    }, [router]);
 
-                                <Text
-                                    fontSize={["lg", "xl"]}
-                                    lineHeight={[8, 10]}
-                                    p={0}
-                                >
-                                    {item}
-                                </Text>
-                            </Flex>
-                        ))}
-                    </VStack>
-                </Flex>
-                <Button
-                    onClick={() => {
-                        router.push(
-                            user ? "/explore" : "/loginRegister?login=true"
-                        );
-                    }}
-                    variant="primary"
+    useEffect(() => {
+        isUserLoggedIn();
+    }, [isUserLoggedIn]);
+
+    return (
+        <>
+            {!loading ? (
+                <Flex
+                    h="full"
+                    m={0}
+                    px={[4, 8]}
+                    direction="column"
+                    alignItems="center"
                 >
-                    Get Started
-                </Button>
-            </Flex>
-        </Flex>
+                    <Flex
+                        mt={8}
+                        mb={4}
+                        w={["250px", "350px", "550px"]}
+                        alignItems="center"
+                        height={["80px", "110px", "140px"]}
+                        fontSize={["3xl", "4xl", "5xl"]}
+                        fontFamily="brand"
+                        backgroundImage={"url(/logo_1x.png)"}
+                        backgroundSize={["100% 100%"]}
+                        backgroundRepeat="no-repeat"
+                    >
+                        <Text w="full" textAlign="center">
+                            PHONIQUEST
+                        </Text>
+                    </Flex>
+                    <ScaleFade initialScale={0.9} in={!loading}>
+                        <Flex
+                            direction="column"
+                            alignItems="center"
+                            gap={6}
+                            p={6}
+                            h="full"
+                            w="full"
+                        >
+                            <Text fontSize={["2xl", "3xl"]}>
+                                Welcome to Phoniquest
+                            </Text>
+                            <Flex w="full" maxW="500px" pb={10}>
+                                <VStack gap={4}>
+                                    {listItems.map((item) => (
+                                        <Flex
+                                            alignItems="center"
+                                            key={item}
+                                            gap={2}
+                                            justifyContent="flex-start"
+                                            w="full"
+                                        >
+                                            <Box
+                                                w="30px"
+                                                h="15px"
+                                                minW="30px"
+                                                backgroundImage={
+                                                    "url(/logo_1x.png)"
+                                                }
+                                                backgroundSize="100% 100%"
+                                                backgroundRepeat="no-repeat"
+                                            ></Box>
+
+                                            <Text
+                                                fontSize={["lg", "xl"]}
+                                                lineHeight={[8, 10]}
+                                                p={0}
+                                            >
+                                                {item}
+                                            </Text>
+                                        </Flex>
+                                    ))}
+                                </VStack>
+                            </Flex>
+                            <Button
+                                onClick={() => {
+                                    router.push(
+                                        user
+                                            ? "/explore"
+                                            : "/loginRegister?login=true"
+                                    );
+                                }}
+                                variant="primary"
+                            >
+                                Get Started
+                            </Button>
+                        </Flex>
+                    </ScaleFade>
+                </Flex>
+            ) : (
+                <Center
+                    h="50vh"
+                    flexDirection="column"
+                    justifyContent="space-between"
+                >
+                    <Flex
+                        position="relative"
+                        left={1}
+                        mt={8}
+                        mb={4}
+                        w={["250px", "350px", "550px"]}
+                        alignItems="center"
+                        height={["80px", "110px", "140px"]}
+                        fontSize={["3xl", "4xl", "5xl"]}
+                        fontFamily="brand"
+                        backgroundImage={"url(/logo_1x.png)"}
+                        backgroundSize={["100% 100%"]}
+                        backgroundRepeat="no-repeat"
+                    >
+                        <Text w="full" textAlign="center">
+                            PHONIQUEST
+                        </Text>
+                    </Flex>
+                    <BouncingDotsLoader />
+                </Center>
+            )}
+        </>
     );
 }
